4b854b35cc82aa3214d57b96df4f1f7989985161
audrilli <audreymeret.lohmann@zhdk.ch>
2022-11-17 10:41:41 +0100

    Add hammer js

--- 03-Homework-futurCities/sketch.js
+++ 03-Homework-futurCities/sketch.js
@@ -3,8 +3,6 @@ var mySvg;
 let drops = [];
 let speed;
 
-
-
 function preload() {
   table = loadTable("future_cities_data_truncated_2.csv", "csv", "header");
   mySvg = loadImage("asset1.svg");
@@ -13,15 +11,12 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   for (let i = 0; i < 50; i++) {
-    drops.push(new Rain(width/2, -1000, 3));
+    drops.push(new Rain(width / 2, -1000, 3));
   }
-
-  
- 
 }
 
 function draw() {
-  background(235,235,254);
+  background(235, 235, 254);
 
   for (let i = 0; i < drops.length; i++) {
     let drop = drops[i];
@@ -32,7 +27,7 @@ function draw() {
   for (let i = 0; i < table.getRowCount(); i++) {
     const city = table.get(i, "current_city");
     const annualPrecipitation = table.get(i, "Annual_Precipitation");
-    const futureAnnualPrecipitation = table.get(i, "future_Annual_Precipitation");
+    const futureAnnualPrecipitation = table.get(i,"future_Annual_Precipitation");
 
     const xPos = 80;
     const startingYPos = 40;
@@ -41,32 +36,27 @@ function draw() {
     const rectWidth2 = futureAnnualPrecipitation / 3;
     const rectHeight = 100;
 
-    fill(0,0,255,50);
-    rect(xPos, startingYPos + (i + 1) * stepY, rectWidth, rectHeight); 
-    rect(xPos, startingYPos + (i + 1) * stepY +10, rectWidth2, rectHeight); 
+    fill(0, 0, 255, 50);
+    rect(xPos, startingYPos + (i + 1) * stepY, rectWidth, rectHeight);
+    rect(xPos, startingYPos + (i + 1) * stepY + 10, rectWidth2, rectHeight);
     push();
-    fill(0,0,255);
+    fill(0, 0, 255);
     textSize(30);
-    text(city,xPos+10,(i+1)* stepY+100);
+    text(city, xPos + 10, (i + 1) * stepY + 100);
     pop();
 
     imageMode(CENTER);
     image(mySvg, 300, (i + 1) * 151, 35, 52);
 
-  
-
     const mouseOverBoundries = {
-     
       minX: xPos,
       maxX: xPos + rectWidth, // startX + width
       minY: startingYPos + (i + 1) * stepY,
-      maxY: startingYPos + (i + 1) * stepY + rectHeight, 
-
-      
+      maxY: startingYPos + (i + 1) * stepY + rectHeight,
     };
 
     const mouseOver = isMouseOverRectangel(mouseOverBoundries);
-    
+
     if (mouseOver) {
       // passing the two texts into the drawing function
       const labelXPos = 332;
@@ -81,34 +71,32 @@ function draw() {
         labelYPos
       );
     }
-    
   }
-  
 
-  fill(0,0,255);
+  fill(0, 0, 255);
   textSize(20);
   text("Annual Precipitation", 80, 147, 200, 100);
   text("Future Annual Precipitation", 80, 742, 300, 100);
 }
 
-function isMouseOverRectangel(boundries){
-  if(mouseX >= boundries.minX &&
+function isMouseOverRectangel(boundries) {
+  if (
+    mouseX >= boundries.minX &&
     mouseX <= boundries.maxX &&
-    mouseY >= boundries.minY && 
+    mouseY >= boundries.minY &&
     mouseY <= boundries.maxY
-    ){
-      return true;
-    }else{
-      return false;
-    }
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function drawLabel(precipitation,futurePrecipitation,x,y){
-  const textBoxSize =200;
-  let textcolor = fill(0,0,255);
+function drawLabel(precipitation, futurePrecipitation, x, y) {
+  const textBoxSize = 200;
+  let textcolor = fill(0, 0, 255);
   textcolor;
-  text(precipitation,x,y,textBoxSize,textBoxSize);
-  text(precipitation,x,y+25,textBoxSize,textBoxSize);
+  text(precipitation, x, y, textBoxSize, textBoxSize);
+  text(precipitation, x, y + 25, textBoxSize, textBoxSize);
 }
 console.log("code end");
-    
